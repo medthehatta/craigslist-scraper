51c1e017e29c789ccd220173ed0cad7c6c26bd7c
Med Mahmoud <medthehatta@gmail.com>
2013-05-23 16:06:39 -0400

    First pass refactor for title context

--- word_analyze.py
+++ word_analyze.py
@@ -12,33 +12,32 @@ try:
 
     print("Data loaded from pickle files.")
 
+    # Get the database data
+    db = sqlite3.connect('entries.db')
+    df = frame_query('select * from entries',db)
+
+
 except Exception:
 
     print("Data not found.  Regenerating histograms...")
 
-    # Get the data
-    db = sqlite3.connect('entries.db')
-    df = frame_query('select * from entries',db)
-
     # Initialize the storage
-    corpus=Counter()
+    texts = Counter()
     sub_texts={}
+    titles=Counter()
+    sub_title={}
 
-    # Retrieve id and text only
-    id_and_text_0 = df[['id','text']].T
-    len_id_and_text = len(id_and_text_0)
-    id_and_text = id_and_text_0.iteritems()
+    records = df[['id','title','text']].T
+    records_len = len(records)
 
     # Initialize the "word" regexp
     words_rx = re.compile(r'[a-zA-Z\-]+')
 
     # Compute all the histograms
-    for (i,(id,text)) in zip(count(),id_and_text):
-        word_list = words_rx.findall(str(text).lower())
-        sub_text=Counter(word_list)
-        sub_texts[id]=sub_text
-        corpus+=sub_text
-        if i%100==0: print("{}/{}".format(i,len_id_and_text))
+    for (i,(id,title,text)) in zip(count(),records.iteritems()):
+        sub_texts[id] = word_histogram(text,[texts])
+        sub_title[id] = word_histogram(title,[titles,texts])
+        if i%100==0: print("{}/{}".format(i,records_len))
 
     print("Histograms generated.  Saving...")
 
@@ -47,8 +46,24 @@ except Exception:
         pickle.dump(corpus,f)
     with open("sub_texts.pkl",'wb') as f:
         pickle.dump(sub_texts,f)
+    with open("sub_titles.pkl",'wb') as f:
+        pickle.dump(sub_title,f)
 
     print("Done.")
+
+
+def word_histogram(text,contexts):
+    """
+    Makes a histogram of the words in a text, and includes the word counts in
+    global contexts.
+    """
+    word_list = re.findall(r'[a-zA-Z\-]+', str(text).lower())
+    word_histogram = Counter(word_list) 
+
+    for context in contexts:
+        context += word_histogram
+
+    return word_histogram
 
 
 
@@ -64,9 +79,7 @@ def idf(num,den):
 
     vals = []
     for k in num:
-        d = den.get(k) 
-        if d is None: raise KeyError("den does not contain {}".format(k))
-        vals.append([num[k]/float(d),k])
+        vals.append([num[k]/float(den[k]),k])
 
     return [a[1] for a in sorted(vals,key=lambda x:-x[0])]
 
