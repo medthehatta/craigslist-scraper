6d633426b3e8b529f9249fe6f187fc6c75bb6ac1
Med Mahmoud <med@temple.edu>
2013-05-25 17:34:10 -0400

    Add convenience tool to query for strings in ad text

---
+++ standard_query.py
@@ -0,0 +1,28 @@
+import sqlite3
+from pandas.io.sql import read_frame
+import sys
+
+if __name__=='__main__':
+    db = sqlite3.connect('entries.db') 
+    query_template = ("SELECT id,title,url,date(date) "
+                      "FROM entries "
+                      "WHERE date(date)>date('now','-5 day') "
+                      "AND text LIKE '%{}%'"
+                      "ORDER BY date(date)")
+    c = db.cursor()
+    for keyword in sys.argv[1:]:
+        print()
+        print(">>> {}".format(keyword))
+        df = c.execute(query_template.format(keyword))
+        da = df.fetchall()
+
+        for line in da:
+            (id,title,url,date) = line
+            try:
+                print("({}) {}\n  {} - {}".format(id,url,title,date))
+            except UnicodeEncodeError:
+                print("({}) {}\n  {} - {}".format(id,url,"[unicode error]",date))
+                
+            
+
+
